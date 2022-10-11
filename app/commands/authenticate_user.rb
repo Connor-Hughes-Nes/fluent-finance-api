50c69466e9a279bbb70156046f5b18f9f5824ce0
Connor-Hughes-Nes <chughes@neslotech.co.za>
2022-10-11 17:52:27 +0200

    Implement simple command

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,33 @@
+# define a command class
+class AuthenticateUser
+  # put SimpleCommand before the class' ancestors chain
+  prepend SimpleCommand
+  include ActiveModel::Validations
+
+  # optional, initialize the command with some arguments
+  def initialize(email, password, role)
+    @email = email
+    @password = password
+    @role = role
+  end
+
+  ## config/locales/en.yml
+  # en:
+  #   activemodel:
+  #     errors:
+  #       models:
+  #         authenticate_user:
+  #           failure: Wrong email or password
+
+
+  # mandatory: define a #call method. its return value will be available
+  #            through #result
+  def call
+    if (user = User.find_by(email: @email)&.authenticate(@password))
+      return user
+    else
+      errors.add(:base, :failure)
+    end
+    nil
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def create
+    # initialize and execute the command
+    # NOTE: `.call` is a shortcut for `.new(args).call`
+    command = AuthenticateUser.call(session_params[:email], session_params[:password])
+
+    # check command outcome
+    if command.success?
+      # command#result will contain the user instance, if found
+      session[:user_token] = command.result.secret_token
+      redirect_to root_path
+    else
+      flash.now[:alert] = t(command.errors.full_messages.to_sentence)
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
