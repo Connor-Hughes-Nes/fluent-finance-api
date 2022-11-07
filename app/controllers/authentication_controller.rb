230d0ef86f3d9f2024a08f14bef9492b54a9dac6
Connor-Hughes-Nes <chughes@neslotech.co.za>
2022-11-07 18:52:49 +0200

    FF: Fix authentication completed

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -9,20 +9,20 @@ class AuthenticationController < ApplicationController
 
   # POST /login
   def authenticate
-    @user = User.find_by(params[:id])
+    @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = JsonWebToken.encode(id: @user.id)
+      token = JsonWebToken.encode(id: @user.id, email: @user.email, role: @user.role)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                      first_name: @user.first_name, last_name: @user.last_name }, status: :ok
     else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+      render json: { error: 'Unauthorized' }, status: :unauthorized
     end
   end
 
   private
 
   def login_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,8 +50,11 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(
-      :first_name, :last_name, :email, :password, :password_confirmation
+    # params.permit(
+    #   :first_name, :last_name, :email, :password, :password_confirmation
+    # )
+    params.require(
+      :user).permit(:first_name, :last_name, :email, :password, :password_confirmation
     )
   end
 end
